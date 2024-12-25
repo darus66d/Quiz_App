df621b9bf6ffc31c349e9a8aaade455d3386d93a
Darus Salam <darussalam66d@gmail.com>
2024-12-25 10:23:52 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+
+
+void main(){
+  runApp (const QuizApp());
+}
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors:[
+                  Color.fromARGB(255, 78, 13, 151),
+                  Color.fromARGB(255, 107, 15, 168),
+                ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomLeft,
+            ),
+          ),
+            child: const StartScreen()),
+      )
+    );
+  }
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+        child: Column(
+          children: [
+            Image.asset("image1.jpg",width: 100,height: 100,),
+
+          ],
+        ),
+    );
+  }
+}
