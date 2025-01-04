ee86fed5fbab7946c884fb9d86e316bf2e018e5c
Darus Salam <darussalam66d@gmail.com>
2025-01-04 12:06:32 +0600

    update

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questios.dart';
 import 'package:quiz_app/question_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
@@ -11,19 +12,26 @@ class Quiz extends StatefulWidget {
 
 class _QuizState extends State<Quiz> {
 
-  final List<String> selectedAnswers = [];
+   List<String> selectedAnswers = [];
 
 
   var activeScreen = "start-Screen";
 
   void switchScreen (){
     setState(() {
+      selectedAnswers=[];
       activeScreen = "questions-screen";
     });
   }
 
   void chooseAnswer(String answer){
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length){
+      setState(() {
+        activeScreen = "start-Screen";
+      });
+    }
   }
 
   // Widget? activeScreen;
